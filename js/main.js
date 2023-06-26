9d6ae233780b6934f4097cfa5aac4dc0a1549f6b
João Gabryel Dos Santos Pereira <133254442+jgabryeldev@users.noreply.github.com>
2023-06-26 16:16:39 -0300

    colocando no for
    Criado as condicionais e colocadas dentro do for.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,33 @@
 
 titulo.textContent = "Modifica o texto da página";*/
 
-let paciente = document.querySelector(".primeiro-paciente");
-let peso = paciente.querySelector(".info-peso");
-peso = peso.textContent;
-let altura = paciente.querySelector(".info-altura");
-altura = altura.textContent;
-let imc = paciente.querySelector(".info-imc");
-imc.textContent = peso/ (altura*altura);
+let pacientes = document.querySelectorAll(".paciente");
+
+for(let i = 0; i<pacientes.length; i++){
+
+    let paciente = pacientes[i];
+    let peso = paciente.querySelector(".info-peso");
+    peso = peso.textContent;
+    let altura = paciente.querySelector(".info-altura");
+    altura = altura.textContent;
+    let imc = paciente.querySelector(".info-imc");
+    imc.textContent = peso/(altura*altura);
+    let pesoValido = true;
+    let alturaValida = true;
+
+    if(peso>=1000 || peso<=0)
+    {
+        imc.textContent = "Peso Inválido!";
+        pesoValido = false;
+    }
+    if(altura>=3 || altura<=0)
+    {
+        imc.textContent = "Altura Inválida!";
+        alturaValida= false;
+    }
+
+    if(pesoValido && alturaValida){
+        let imcCalculo = peso/(altura*altura);
+        imc.textContent = imcCalculo.toFixed(2);
+    }
+}

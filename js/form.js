a7a1aff6dc0f12c3ea9d7400bfd0c3ede90bdbca
João Gabryel Dos Santos Pereira <133254442+jgabryeldev@users.noreply.github.com>
2023-07-03 18:14:50 -0300

    Melhoras no código

--- js/form.js
+++ js/form.js
@@ -3,33 +3,56 @@ let botaoAdicionaPaciente = document.querySelector("#adicionar-paciente");
 botaoAdicionaPaciente.addEventListener("click", function adicionaPaciente(Event){
     Event.preventDefault();
     let formulario = document.querySelector("#form-adiciona");
-    
-    let nome = formulario.nome.value;
-    let peso = formulario.peso.value;
-    let altura = formulario.altura.value;
-    let gordura = formulario.gordura.value;
+    let paciente = dadosPaciente(formulario);
+    let pacienteTr = criaTr(paciente);
+    /*Adiciona à tabela do meu HTML*/
+    let tabela = document.querySelector("#tabela");
+    tabela.appendChild(pacienteTr);
+    formulario.reset();
+}
+)
 
+/*Cria o objeto paciente*/
+function dadosPaciente(formulario){
+    let paciente = {
+        nome: formulario.nome.value,
+        peso: formulario.peso.value,
+        altura: formulario.altura.value,
+        gordura: formulario.gordura.value,
+        imc: calculaImc(formulario.peso.value, formulario.altura.value)
+    }
+    return paciente;
+}
+
+function criaTr(paciente) {
+    /*Cria os elementos da tabela*/
     let pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
     let nomeTd = document.createElement("td");
     let pesoTd = document.createElement("td");
     let alturaTd = document.createElement("td");
     let gorduraTd = document.createElement("td");
     let imcTd = document.createElement("td");
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso, altura);
+    /*Extrai o conteúdo de texto do paciente*/
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
 
+    /*Adiciona classe ao td*/
+    nomeTd.classList.add("info-nome");
+    pesoTd.classList.add("info-peso");
+    alturaTd.classList.add("info-altura");
+    gorduraTd.classList.add("info-gordura");
+    imcTd.classList.add("info-imc");
+    
+    /*Põe os elementos como filho da minha Tr*/
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
-
-    let tabela = document.querySelector("#tabela");
-
-    tabela.appendChild(pacienteTr);
+    return pacienteTr;
 }
-)
